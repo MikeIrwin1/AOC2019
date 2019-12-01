42dd7bacfab83a9181aabc22e31ef01b203df0df
Michael Irwin <mike.irwin1@gmail.com>
2019-12-01 22:08:50 +0000

    refactor day_1 to allow solution of both part 1 and 2 to be displayed

--- src/day_1.py
+++ src/day_1.py
@@ -4,15 +4,24 @@ with open('input\\day_1.txt', 'r') as f:
 def module_fuel(module_mass):
     return module_mass//3 - 2
 
-total_fuel = 0
-additional_fuel = 0
+def solve_part_1(data):
+    total_fuel = 0
+    for item in data:
+        total_fuel += module_fuel(item)
+    return total_fuel
 
-for item in data:
-    additional_fuel = module_fuel(item)
+def solve_part_2(data):
+    total_fuel = 0
+    additional_fuel = 0
+    for item in data:
+        additional_fuel = module_fuel(item)
 
-    while additional_fuel > 0:
-        total_fuel += additional_fuel
-        additional_fuel = module_fuel(additional_fuel)
+        while additional_fuel > 0:
+            total_fuel += additional_fuel
+            additional_fuel = module_fuel(additional_fuel)
+    return total_fuel
 
-
-print(total_fuel)
+part_1 = solve_part_1(data)
+part_2 = solve_part_2(data)
+print(part_1)
+print(part_2)
