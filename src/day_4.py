f1c2fcb56bee575b0e47684956f426ef2a04bb5a
Michael Irwin <mike.irwin1@gmail.com>
2019-12-04 19:22:02 +0000

    part 2 complete and refactored methods from part 1

--- src/day_4.py
+++ src/day_4.py
@@ -3,19 +3,25 @@ u_limit = 781584
 
 num_passwords = 0 
 def validate_dupe(number):
-    num_string = str(number)
-    if num_string[0] == num_string[1] or num_string[1] == num_string[2] or num_string[2] == num_string[3] or num_string[3] == num_string[4] or num_string[4] == num_string[5]:
+    if number[0] == number[1] or number[1] == number[2] or number[2] == number[3] or number[3] == number[4] or number[4] == number[5]:
         return True
     return False
 
 def validate_increase(number):
-    num_string = str(number)
-    if num_string[0] <= num_string[1] <= num_string[2] <= num_string[3] <= num_string[4] <= num_string[5]:
+    if number[0] <= number[1] <= number[2] <= number[3] <= number[4] <= number[5]:
         return True
     return False
 
-for attempt in range(l_limit,u_limit):
-    if validate_dupe(attempt) and validate_increase(attempt):
+def large_groups(number):
+    for digit in number:
+        if number.count(digit) == 2:
+            return True
+    return False
+
+
+for attempt in range(l_limit,u_limit+1):
+    password = str(attempt)
+    if validate_dupe(password) and validate_increase(password) and large_groups(password):
         num_passwords += 1
 
 print(num_passwords)
